3bbcb79b96bd32e7bc33fff8c9d4b28316b6a16d
Tomas Molina <jtml.mass@gmail.com>
2023-03-03 22:59:06 -0600

    Creating Card Component
    Co-authored-by: ariasmarmando <ariasmarmando@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,47 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import CardAnime from './components/Card';
 import './App.css'
+import { Container } from '@nextui-org/react';
+import { Col, Row, } from "@nextui-org/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-      <h1>AnimeChan</h1>
-      <CardAnime />
+      <Container fluid>
+          {/* <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://reactjs.org" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div> */}
+        <h1>AnimeChan</h1>
+        <Row>
+          <Col>
+            <CardAnime 
+              imageBg="https://media.kitsu.io/anime/poster_images/1/original.jpg?1591625948"
+              imageBgAlt="imageBgAlt"
+              avRating="avRating"
+              canonicalTitle="canonicalTitle"
+              ageRating="ageRating"
+              subtype="subtype"
+              popularityRank="popularityRank"
+            />
+          </Col>
+          <Col>
+           <CardAnime 
+              imageBg="https://media.kitsu.io/anime/poster_images/1/original.jpg?1591625948"
+              imageBgAlt="imageBgAlt"
+              avRating="avRating"
+              canonicalTitle="canonicalTitle"
+              ageRating="ageRating"
+              subtype="subtype"
+              popularityRank="popularityRank"
+            />
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,74 @@
-import React from 'react'
+import React from 'react';
+import { Card, Col, Row, Button, Text } from "@nextui-org/react";
 
-const CardAnime = () => {
+const CardAnime = ({imageBg, imageBgAlt, avRating, canonicalTitle, ageRating, subtype, popularityRank}) => {
   return (
-    <div>CardAnime</div>
+    <Card css={{ w: "40vh", h: "400px" }}>
+    <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+      <Col>
+        <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
+          {avRating}
+        </Text>
+        <Text h1 color="white" transform="uppercase">
+          {canonicalTitle}
+        </Text>
+        <Text h3 color="white" transform="uppercase">
+          {subtype}
+        </Text>
+      </Col>
+    </Card.Header>
+    <Card.Body css={{ p: 0 }}>
+      <Card.Image
+        // original
+        src={imageBg}
+        objectFit="cover"
+        width="100%"
+        height="100%"
+        alt={imageBgAlt}
+      />
+    </Card.Body>
+    <Card.Footer
+      isBlurred
+      css={{
+        position: "absolute",
+        bgBlur: "#0f111466",
+        borderTop: "$borderWeights$light solid $gray800",
+        bottom: 0,
+        zIndex: 1,
+      }}
+    >
+      <Row>
+        <Col>
+          <Text color="#d1d1d1" size={12}>
+            {ageRating}
+          </Text>
+        </Col>
+        <Col>
+          <Text color="#d1d1d1" size={12}>
+            {popularityRank}
+          </Text>
+        </Col>
+        <Col>
+          <Row justify="flex">
+            <Button
+              flat
+              auto
+              css={{ color: "#94f9f0", bg: "#94f9f026"}}
+            >
+              <Text
+                css={{ color: "inherit" }}
+                size={12}
+                weight="bold"
+                transform="uppercase"
+              >
+                ADD ->
+              </Text>
+            </Button>
+          </Row>
+        </Col>
+      </Row>
+    </Card.Footer>
+  </Card>
   )
 }
 
